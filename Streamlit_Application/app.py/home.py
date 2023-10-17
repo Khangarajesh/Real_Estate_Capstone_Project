d771a965fef37399e2ef82c0ab2396a3e024af17
Rajesh Khangar <107788892+Khangarajesh@users.noreply.github.com>
2023-10-17 09:02:22 +0530

    Add files via upload

---
+++ Streamlit_Application/app.py/home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+st.set_page_config(page_title="Home")
+st.title("Welcome to Khangar Analytics")
+
+st.write('''The Objective of Khangar Analytics is to design, develop, and deploy an integrated Real Estate Intelligence System (REIS) that leverages advanced data science techniques 
+for accurate real estate price prediction, in-depth market analysis, and personalized property recommendations. 
+The system will utilize machine learning regression models to forecast property prices, conduct comprehensive exploratory data analysis (EDA) 
+to uncover meaningful insights about market trends and customer preferences, and implement a recommendation engine powered by collaborative filtering
+ algorithms. The primary goal is to empower homebuyers, sellers, and real estate professionals with data-driven insights, 
+ enabling them to make informed decisions, optimize property investments, and enhance overall user satisfaction in the real estate market.''')
